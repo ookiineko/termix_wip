dc1eb7d2af9fc9f8acacc7e051d7d985f31f3cb6
Ookiineko <chiisaineko@protonmail.com>
2023-12-27 23:54:11 +0800

    ldr: main: update src desc
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- ldr/main.c
+++ ldr/main.c
@@ -1,5 +1,5 @@
 /*
-  main.c - Termix program loader
+  main.c - ELF loader
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
