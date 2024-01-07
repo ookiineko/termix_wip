07b6eb0e5a4305386d5e07aac0a2d3d31dc11724
Ookiineko <chiisaineko@protonmail.com>
2024-01-07 20:06:48 +0800

    tests: linux: syscalls: correct license for this file
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- tests/lib/linux/syscalls.h
+++ tests/lib/linux/syscalls.h
@@ -1,18 +1,26 @@
 /*
-  syscalls.h - Syscall wrappers
+  syscalls.h - Syscall wrappers from musl libc
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  Copyright © 2005-2020 Rich Felker, et al.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef TERMIX_TESTS_HOSTLIB_SYSCALLS
@@ -20,8 +28,6 @@
 
 #include <sys/types.h>
 #include <syscall.h>
-
-// syscall wrappers from musl
 
 #ifdef __i386__
   static inline long __syscall1(long n, long a1) {
